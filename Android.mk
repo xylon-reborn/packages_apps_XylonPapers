b9202307ddda803bf10626da0c9719283648e1bd
krabappel2548 <krabappel2548@gmail.com>
2013-11-18 11:20:58 +0100

    Update

--- Android.mk
+++ Android.mk
@@ -14,16 +14,14 @@
 # limitations under the License.
 #
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4
+LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
-LOCAL_PACKAGE_NAME := Xylonpapers
+LOCAL_PACKAGE_NAME := XylonPapers
+LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
